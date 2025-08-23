fa56c1f970e700b7bdd8bde59a0e93a994636b01
Azeez Ololade <ololadeazeez.m@gmail.com>
2025-08-23 10:46:20 +0100

    Initial commit

--- contact-message.js
+++ contact-message.js
@@ -7,7 +7,8 @@ import bodyParser from "body-parser";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000; 
+
 
 app.use(cors())
 
